8af43cab89dac279a5e6ae471de1ef53c58bd54c
Bednar <gbed24@gmail.com>
2024-07-31 18:39:37 +0200

    feat(Camera): Added view switching by body and Center of Mass

--- src/entities.py
+++ src/entities.py
@@ -4,9 +4,28 @@ vec = pygame.Vector2
 GRAVITY_CONSTANT = 10.0
 TRAIL_LIFE = 600
 TRAIL_RADIUS = 2
-DISPLAY_WIDTH = 1920
-DISPLAY_HEIGHT = 1080
-SCREEN_CENTER = vec((0, DISPLAY_HEIGHT / 2))
+DEFAULT_DENSITY = 0.5
+DISPLAY_WIDTH = 3840
+DISPLAY_HEIGHT = 2160
+SCREEN_CENTER = vec((DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2))
+
+COLORS = {
+    "White": (255, 255, 255),
+    "Grey": (128, 128, 128),
+    "Red": (255, 0, 0),
+    "Green": (0, 255, 0),
+    "Blue": (0, 0, 255),
+    "DarkGrey": (50, 50, 50)
+}
+
+
+class Info:
+    @staticmethod
+    def draw_center_of_mass(pos: vec, surface: pygame.Surface):
+        pygame.draw.line(surface=surface, color=(128, 128, 0), start_pos=pos + vec((-10, 0)),
+                         end_pos=pos + vec((10, 0)), width=2)
+        pygame.draw.line(surface=surface, color=(128, 128, 0), start_pos=pos + vec((0, -10)),
+                         end_pos=pos + vec((0, 10)), width=2)
 
 
 class Trail:
@@ -15,7 +34,7 @@ class Trail:
         self.color = color
         self.life = TRAIL_LIFE
 
-    def update(self, surface: pygame.Surface, offset: vec) -> bool:
+    def update(self) -> bool:
         if self.life <= 0:
             return True
         else:
@@ -28,20 +47,25 @@ class Trail:
             new_g = int(g * value)
             new_b = int(b * value)
             self.color = (new_r, new_g, new_b)
-            pygame.draw.circle(surface=surface, color=self.color, center=self.pos + offset, radius=TRAIL_RADIUS)
             return False
+
+    def refresh(self, surface: pygame.Surface, offset: vec):
+        pygame.draw.circle(surface=surface, color=self.color, center=self.pos - offset, radius=TRAIL_RADIUS)
 
 
 class Planet:
-    def __init__(self, pos: vec, vel: vec, mass: float, radius: float, color: tuple):
+    def __init__(self, pos: vec, vel: vec, mass: float, color: tuple, radius: float = None):
         self.pos = pos
         self.vel = vel
         self.mass = mass
-        self.radius = radius
+        if radius:
+            self.radius = radius
+        else:
+            self.radius = calculate_radius_from_mass(density=DEFAULT_DENSITY, mass=self.mass)
         self.color = color
 
     def refresh(self, surface: pygame.Surface, offset: vec):
-        pygame.draw.circle(surface=surface, color=self.color, center=self.pos + offset, radius=self.radius)
+        pygame.draw.circle(surface=surface, color=self.color, center=self.pos - offset, radius=self.radius)
 
     def move(self, bounce: bool) -> Trail:
         previous_pos = self.pos.copy()
@@ -49,7 +73,7 @@ class Planet:
         if bounce:
             if self.pos.x < 0 or self.pos.x > DISPLAY_WIDTH:
                 self.vel.x = -self.vel.x
-            if self.pos.y < 0 or self.pos.y > DISPLAY_HEIGHT:
+            if self.pos.y< 0 or self.pos.y> DISPLAY_HEIGHT:
                 self.vel.y = -self.vel.y
 
         return Trail(pos=previous_pos, color=self.color)
@@ -90,7 +114,7 @@ def calculate_center_of_mass(planet_list: list) -> vec:
     total_mass = 0.0
     for planet in planet_list:
         x_weighted += planet.pos.x * planet.mass
-        y_weighted -= planet.pos.y * planet.mass
+        y_weighted += planet.pos.y * planet.mass
         total_mass += planet.mass
     return vec((x_weighted / total_mass, y_weighted / total_mass))
 
@@ -104,9 +128,18 @@ def combine_colors(a: tuple, b: tuple) -> tuple:
     return tuple(c)
 
 
+def print_debug_values(values_to_print: list, font: pygame.font.SysFont, surface: pygame.Surface):
+    text_y = 10
+    for strings in values_to_print:
+        text = font.render(strings, True, (255, 255, 255))
+        text_rect = text.get_rect(topleft=(10, text_y))
+        surface.blit(text, text_rect)
+        text_y += text_rect.h + 1
+
+
 def check_collisions(planet_list: list) -> list:
     collisions_detected = True
-    while(collisions_detected):
+    while collisions_detected:
         collisions_detected = False
         for planet_a in planet_list:
             for planet_b in planet_list:
--- src/run.py
+++ src/run.py
@@ -1,9 +1,11 @@
 import sys
 import pygame
 from pygame.locals import *
+import ctypes
+ctypes.windll.user32.SetProcessDPIAware()
 
 from entities import (Planet, vec, calculate_planet_forces, check_collisions, DISPLAY_WIDTH, DISPLAY_HEIGHT,
-                      calculate_center_of_mass, SCREEN_CENTER)
+                      calculate_center_of_mass, SCREEN_CENTER, print_debug_values, COLORS, Info)
 
 pygame.init()
 print(f"\n\nGravity")
@@ -16,28 +18,19 @@ standard_font = pygame.font.SysFont("Arial", 24)
 pygame.display.set_caption("Gravity")
 FPS = 60
 bounce_screen = False
-
-
-# fonts
-COLORS = {
-    "White": (255, 255, 255),
-    "Grey": (128, 128, 128),
-    "Red": (255, 0, 0),
-    "Green": (0, 255, 0),
-    "Blue": (0, 0, 255),
-    "DarkGrey": (50, 50, 50)
-}
+is_physics_enabled = True
 
 # test data
 Planets = []
 Trails = []
-Planets.append(Planet(pos=vec((950, 100)), vel=vec((4, 0)), mass=1.0, radius=5.0, color=COLORS["Red"]))
-Planets.append(Planet(pos=vec((500, 500)), vel=vec((0, -8)), mass=1000.0, radius=20.0, color=COLORS["White"]))
-Planets.append(Planet(pos=vec((950, 300)), vel=vec((-4, -5)), mass=1.0, radius=5.0, color=COLORS["Green"]))
-Planets.append(Planet(pos=vec((950, 330)), vel=vec((6, 0)), mass=0.03, radius=2.0, color=COLORS["Grey"]))
-Planets.append(Planet(pos=vec((1300, 10)), vel=vec((0, 0)), mass=10.0, radius=5.0, color=COLORS["Green"]))
-Planets.append(Planet(pos=vec((600, 500)), vel=vec((1, 7)), mass=1000.0, radius=20.0, color=COLORS["White"]))
+Planets.append(Planet(pos=vec((2000, 1000)), vel=vec((8, 0)), mass=1000.0, color=COLORS["Red"]))
+Planets.append(Planet(pos=vec((2000, 900)), vel=vec((-8, 0)), mass=1000.0, color=COLORS["Blue"]))
+Planets.append(Planet(pos=vec((2000, 100)), vel=vec((2.5, 0)), mass=100.0, color=COLORS["Grey"]))
 
+center_of_mass_pos = vec((0, 0))
+view_id = 0
+views_amount = 0
+view_offset = vec((0, 0))
 while True:
     # read keys
     for event in pygame.event.get():
@@ -50,32 +43,55 @@ while True:
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+            elif event.key == K_SPACE:
+                is_physics_enabled = not is_physics_enabled
+            elif event.key == K_TAB:
+                view_id += 1
 
     display_surface.fill((0, 0, 0))
+    views_amount = len(Planets) + 1
+    if view_id > views_amount:
+        view_id = 0
 
-    calculate_planet_forces(Planets)
-    Planets = check_collisions(Planets)
-    new_offset = calculate_center_of_mass(Planets)
+    if is_physics_enabled:
+        calculate_planet_forces(Planets)
+        Planets = check_collisions(Planets)
 
-    for index, trail in enumerate(Trails):
-        is_flagged_for_deletion = trail.update(display_surface, new_offset + SCREEN_CENTER)
-        if is_flagged_for_deletion:
-            Trails.remove(trail)
+        center_of_mass_pos = calculate_center_of_mass(Planets)
+
+        for trail in Trails:
+            is_flagged_for_deletion = trail.update()
+            if is_flagged_for_deletion:
+                Trails.remove(trail)
+
+        for planet in Planets:
+            new_trail = planet.move(bounce_screen)
+            Trails.append(new_trail)
+
+    if view_id == 0:
+        view_offset = vec((0, 0))
+    elif view_id == 1:
+        view_offset = center_of_mass_pos - SCREEN_CENTER
+    else:
+        for index, planet in enumerate(Planets):
+            if index == view_id - 2:
+                view_offset = planet.pos - SCREEN_CENTER
+                break
 
     for planet in Planets:
-        new_trail = planet.move(bounce_screen)
-        Trails.append(new_trail)
-        planet.refresh(display_surface, new_offset + SCREEN_CENTER)
+        planet.refresh(display_surface, view_offset)
+    for trail in Trails:
+        trail.refresh(display_surface, view_offset)
+
+    Info.draw_center_of_mass(center_of_mass_pos - view_offset, display_surface)
 
     # debug
-    text_y = 10
-    text = standard_font.render(f"Trails: {len(Trails)}", True, COLORS["White"])
-    text_rect = text.get_rect(topleft=(10, text_y))
-    display_surface.blit(text, text_rect)
-    text_y += text_rect.h + 1
-    text = standard_font.render(f"Planets: {len(Planets)}", True, COLORS["White"])
-    text_rect = text.get_rect(topleft=(10, text_y))
-    display_surface.blit(text, text_rect)
+    print_debug_values([f"Trails: {len(Trails)}",
+                        f"Planets: {len(Planets)}",
+                        f"Center of mass: {center_of_mass_pos.x:.0f}, {center_of_mass_pos.y:.0f}",
+                        f"Physics: {is_physics_enabled}",
+                        f"View_id: {view_id}"],
+                       font=standard_font, surface=display_surface)
 
     # refresh screen
     pygame.display.update()
